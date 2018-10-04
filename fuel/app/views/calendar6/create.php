db8d60297a546303efcfb61576635d59da7978d9
Hirokazu Kitamura <171y021@gmail.com>
2018-10-04 12:22:58 +0900

    delete calendar6

--- fuel/app/views/calendar6/create.php
+++
@@ -1,4 +0,0 @@
-<?php echo render('calendar6/_form'); ?>
-
-
-<p><?php echo Html::anchor('calendar6', '戻る',array('class' => 'btn btn-primary')); ?></p>
--- fuel/app/views/calendar6/edit.php
+++
@@ -1,5 +0,0 @@
-<?php echo render('calendar6/_form'); ?>
-<p>
-	<?php echo Html::anchor('calendar6/view/'.$calendar6->id, '詳細',array('class' => 'btn btn-primary')); ?>
-	<?php echo Html::anchor('calendar6', '戻る',array('class' => 'btn btn-primary')); ?>
-	<?php echo Html::anchor('calendar6/delete/'.$calendar6->id, '<i class="icon-trash icon-white"></i> 削除', array('class' => 'btn btn-sm btn-danger', 'onclick' => "return confirm('本当に削除してもよろしいですか？')")); ?></p>
