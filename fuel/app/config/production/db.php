5f2fde95b83db0e06dd223a794b8da317bd37165
Hirokazu Kitamura <171y021@gmail.com>
2018-10-04 09:52:48 +0900

    delete heroku connection

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -10,11 +10,5 @@ return array(
 			'username'   => 'fuel_app',
 			'password'   => 'super_secret_password',
 		),
-		'connection'  => array(
-			'dsn'        => 'mysql:host=bb9f1d2cd164df;dbname=heroku_fd72c924660308f',
-			'username'   => 'bb9f1d2cd164df',
-			'password'   => '9a8948e6',
-		),
-
 	),
 );
