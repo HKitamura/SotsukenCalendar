663426a29dc6b95cf01914aad4a3c8848a2a76d1
Hirokazu Kitamura <171y021@gmail.com>
2018-10-04 10:41:20 +0900

    stagong,production/db.php delete localhost connection

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,11 +6,6 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-		'connection'  => array(
 			'dsn'        => 'mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_fd72c924660308f',
 			'username'   => 'bb9f1d2cd164df',
 			'password'   => '9a8948e6',
